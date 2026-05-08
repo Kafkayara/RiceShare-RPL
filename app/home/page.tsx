41be6956cb487ef2200bd615ea71ae0a19c54e96
Kafkayara <kafkaarya@gmail.com>
2026-05-08 19:41:38 +0700

    init commit

---
+++ app/home/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>🌾 RiceShare</h1>
+      <p>Sistem Manajemen Panen & Bagi Hasil</p>
+
+      <br />
+
+      <ul>
+        <li>✔ Input data panen</li>
+        <li>✔ Hitung otomatis bagi hasil</li>
+        <li>✔ Dashboard visual</li>
+      </ul>
+    </div>
+  )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <div>
-      <h1>🌾 RiceShare</h1>
-      <p>Sistem Manajemen Panen & Bagi Hasil</p>
-
-      <br />
-
-      <ul>
-        <li>✔ Input data panen</li>
-        <li>✔ Hitung otomatis bagi hasil</li>
-        <li>✔ Dashboard visual</li>
-      </ul>
-    </div>
-  )
+  redirect("/login")
 }
